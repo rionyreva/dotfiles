ef6b991912e5c9b1161988e70209b7695ffc8679
Avery Noir <rionyreva@duck.com>
2024-10-11 07:27:08 +0300

    update

---
+++ nixos-unstable/plasma.nix
@@ -0,0 +1,27 @@
+# plasma.nix
+
+{ config, pkgs, lib, ... }:
+
+{
+
+  ## KDE Plasma 6 Desktop Environment. ##
+
+  # Enable the KDE Plasma 6 Desktop Environment.
+
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Excluding some KDE Plasma applications from the default install
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    plasma-browser-integration
+  ];
+
+  # GTK themes are not applied in Wayland applications / Window Decorations missing / Cursor looks different
+  programs.dconf.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+}
