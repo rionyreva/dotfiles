fed4ff20a5f22731dde19824018851ce54fb45c2
Avery Noir <rionyreva@duck.com>
2024-10-17 13:45:35 +0300

    Update install-flatpak-apps.sh

--- flatpak/install-flatpak-apps.sh
+++ flatpak/install-flatpak-apps.sh
@@ -4,23 +4,23 @@
 apps=(
   com.github.tchx84.Flatseal
   io.github.giantpinkrobots.flatsweep
-  org.keepassxc.KeePassXC
-  com.tutanota.Tutanota
-  io.github.shiftey.Desktop
-  com.brave.Browser
-  io.gitlab.librewolf-community
-  org.torproject.torbrowser-launcher
-  dev.vencord.Vesktop
-  com.modrinth.ModrinthApp
-  org.prismlauncher.PrismLauncher
+  # org.keepassxc.KeePassXC
+  # com.tutanota.Tutanota
+  # io.github.shiftey.Desktop
+  # com.brave.Browser
+  # io.gitlab.librewolf-community
+  # org.torproject.torbrowser-launcher
+  # dev.vencord.Vesktop
+  # com.modrinth.ModrinthApp
+  # org.prismlauncher.PrismLauncher
 	
 	# Jagex Launcher
 	# This repo in automatically added via flatpak.nix file!
 	# flatpak remote-add --if-not-exists JagexLauncher https://jagexlauncher.flatpak.mcswain.dev/JagexLauncher.flatpakrepo
-	# org.freedesktop.Platform.Compat.i386/x86_64/23.08
-	# org.freedesktop.Platform.GL32.default/x86_64/23.08
-	# com.jagex.Launcher
-	# com.jagex.Launcher.ThirdParty.RuneLite
+	org.freedesktop.Platform.Compat.i386/x86_64/23.08
+	org.freedesktop.Platform.GL32.default/x86_64/23.08
+	com.jagex.Launcher
+	com.jagex.Launcher.ThirdParty.RuneLite
 )
 
 # Install each application system-wide.
